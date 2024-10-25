1b9e0f895987f0a29a414dcf01c1d7bcbda88bf6
Admin <Admin@PRIYA-4year>
2024-10-25 15:19:07 +0530

    Second commit

--- C2TC_PRIYADHARSHINI/src/com/tnsc/ifet/dayone/FirstProgram.java
+++ C2TC_PRIYADHARSHINI/src/com/tnsc/ifet/dayone/FirstProgram.java
@@ -2,6 +2,9 @@ package com.tnsc.ifet.dayone;
 
 public class FirstProgram {
 	public static void main (String[] args) {
+		int number =9;  // identifiers
+		System.out.println("value of the number is:"+number);
+		
 		System.out.println("Welcome to IFET College");
 		
 	
